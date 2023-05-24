2a4eea7a5f4ebdecdb322ea8e9f724df234b10b9
Fizzika <Fizzika.NET@yandex.ru>
2023-05-24 23:43:24 +0300

    Swagger for version api

--- src/main/java/me/fizzika/tankirating/controller/VersionController.java
+++ src/main/java/me/fizzika/tankirating/controller/VersionController.java
@@ -1,5 +1,6 @@
 package me.fizzika.tankirating.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.fizzika.tankirating.dto.VersionDTO;
 import me.fizzika.tankirating.service.misc.VersionService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Tag(name = "Version", description = "Version API")
 @RestController
 @RequestMapping("/version")
 public class VersionController {
